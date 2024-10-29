4b517852856b5f1418ab75f523a960c48f236c2b
Zac <97338659+ZProLegend007@users.noreply.github.com>
2024-10-29 22:17:34 +0800

    Update install.sh
    Add script install support

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@
 echo "Installing requirements..."
 sudo apt install -y brightnessctl evtest i2c-tools python3 python3-numpy python3-pyaudio python3-libevdev
 
+# Create the /etc/zenlights directory if it does not exist
+sudo mkdir -p /etc/zenlights
+
+# Move scripts to the appropriate directories
+echo "Moving scripts..."
+sudo mv zenlights /usr/local/bin/
+sudo chmod +x /usr/local/bin/zenlights
+sudo mv dance.sh kbvis.py pulse.sh autobacklight.sh spaz.sh /etc/zenlights/
+sudo chmod +x /etc/zenlights/*.sh
+
 # Ask for touchpad backlight support installation
 read -p "Do you want to install touchpad backlight support? (y/n): " yn
 case $yn in
