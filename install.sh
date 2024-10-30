53d0b0bd13d00c560c89e34d5e92da52753d0425
Zac <97338659+ZProLegend007@users.noreply.github.com>
2024-10-30 09:45:44 +0800

    Update install.sh
    sudo test

--- install.sh
+++ install.sh
@@ -12,7 +12,8 @@ cat << "EOF"
 EOF
 # Install necessary requirements
 echo "Installing requirements..."
-sudo apt install -y brightnessctl evtest i2c-tools python3 python3-numpy python3-pyaudio python3-libevdev git
+#sudo 
+apt install -y brightnessctl evtest i2c-tools python3 python3-numpy python3-pyaudio python3-libevdev git
 
 # Clone the repository
 echo "Cloning ZenLights repository..."
@@ -20,14 +21,19 @@ git clone https://github.com/ZProLegend007/ZenLights.git
 cd ZenLights
 
 # Create the /etc/zenlights directory if it does not exist
-sudo mkdir -p /etc/zenlights
+#sudo 
+mkdir -p /etc/zenlights
 
 # Move scripts to the appropriate directories
 echo "Moving scripts..."
-sudo mv zenlights /usr/local/bin/
-sudo chmod +x /usr/local/bin/zenlights
-sudo mv dance.sh kbvis.py pulse.sh autobacklight.sh spaz.sh /etc/zenlights/
-sudo chmod +x /etc/zenlights/*.sh
+#sudo 
+mv zenlights /usr/local/bin/
+#sudo 
+chmod +x /usr/local/bin/zenlights
+#sudo 
+mv dance.sh kbvis.py pulse.sh autobacklight.sh spaz.sh /etc/zenlights/
+#sudo 
+chmod +x /etc/zenlights/*.sh
 
 # Ask for touchpad backlight support installation
 while true; do
@@ -53,7 +59,8 @@ while true; do
     esac
 done
 cd ..
-sudo rm -rf ZenLights
+#sudo 
+rm -rf ZenLights
 rm install.sh
 # clear
 cat << "EOF"
